11fdf2d8146f1195b1affd225b0ed6daf28c96cd
khajamoinuddin_er <khajamoinuddin_er@UKLDN966>
2020-06-23 11:11:34 +0100

    Add project files.

---
+++ WebAppTestGIT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppTestGIT.Startup))]
+namespace WebAppTestGIT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
